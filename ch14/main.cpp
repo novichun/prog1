714810272ec4c3390015004a552ed987fabd662f
Novák Dániel <novakdanci@gmail.com>
2021-03-25 09:11:11 +0100

    2.0

--- ch14/main.cpp
+++ ch14/main.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 struct B1
 {
-	virtual void vf() const {cout<<"B1::vf()"<<endl;}
-	void f() const {cout<<"B1::f()"<<endl;}
+	virtual void vf() const {cout<<"B1::vf"<<endl;}
+	void f() const {cout<<"B1::f"<<endl;}
 	virtual void pvf() =0;
 };
 
 struct D1:B1 
 {	
-	void vf() const override {cout<<"D1::vf()"<<endl;}
-	void f() const {cout<<"D1::f()"<<endl;}
+	void vf() const override {cout<<"D1::vf"<<endl;}
+	void f() const {cout<<"D1::f"<<endl;}
 
 };
 
 struct D2:D1
 {
-	void pvf() override {cout<<"D2:pvf()"<<endl;}
+	void pvf() override {cout<<"D2:pvf"<<endl;}
 };
 
 struct B2
@@ -28,13 +28,13 @@ struct B2
 
 struct D21:B2
 {
-	string datamember="Be nice to me :3";
-	void pvf() override {cout<<datamember<<endl;}
+	string szoveg="Alma";
+	void pvf() override {cout<<szoveg<<endl;}
 };
 
 struct D22:B2
 {
-	int number=13;
+	int number=11;
 	void pvf() override {cout<<number<<endl;}
 };
 
@@ -46,19 +46,6 @@ void f(B2 &ref)
 int main()
 {
 	
-/*	
-	B1 b1;
-	b1.vf();
-	b1.f();
-
-	D1 d1;
-	d1.vf();
-	d1.f();
-
-	B1& bref{d1};
-	bref.vf();
-	bref.f();
-*/
 	D2 d2;
 	d2.f();
 	d2.vf();
